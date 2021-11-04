049c17b00e423b7c9fb922131776ba93f9ec2495
Jakson Moura <jakson.moura@ati.to.gov.br>
2021-11-04 16:59:11 -0300

    Fix grid when limit exceeded

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -38,26 +38,35 @@ function Gallery(){
             </li>
         )
     }
+    const MasonryGrid = () => {
+        try {
+            return (
+                <>
+                    <Masonry
+                        breakpointCols={breakpointColumnsObj}
+                        className="my-masonry-grid"
+                        columnClassName="my-masonry-grid_column"
+                        aria-labelledby="gallery_label">
+                        {gallery.photos.map( (photo, idx) => {
+                            return <PhotoCard key={photo.id} id={idx} photo={photo} />
+                            })
+                        }
+                    </Masonry>
+                    <button className="load-more" onClick={gallery.loadMorePhotos}>Load more photos...</button>
+                </>
+        )
+        }
+        catch {
+            return <div className="error-msg"><p className="emoji">&#x1F615;</p> <p>I'm sorry for this! The API reached it's limit rate. It'll be reset in 1 hour.</p> </div>
+        }
+    }
 
     return(
         <section className="gallery">
             <div className="container">
                 <h1 id="gallery_label">{gallery.searchTerm ? `Photos of "${gallery.searchTerm}"` : "Curated"}</h1>
-
-                <Masonry
-                    breakpointCols={breakpointColumnsObj}
-                    className="my-masonry-grid"
-                    columnClassName="my-masonry-grid_column"
-                    aria-labelledby="gallery_label"
-                    >
-                    {gallery.photos.map( (photo, idx) => {
-                        return <PhotoCard key={photo.id} id={idx} photo={photo} />
-                        })
-                    }
-                </Masonry>
+                <MasonryGrid />
             </div>
-            
-            <button className="load-more" onClick={gallery.loadMorePhotos}>Load more photos...</button>
         </section>
     )
 }
